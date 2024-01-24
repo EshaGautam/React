238e0e549f2938e5994cefa94e7e4de93013e1d9
Esha Gautam <eshagautam0@gmail.com>
2024-01-24 13:15:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import ExpenseItem from "./component/ExpenseItems";
+
+function App() {
+  return (
+    <div >
+    <h1>
+      started Learning react
+    </h1>
+    <ExpenseItem/>
+
+    </div>
+  );
+}
+
+export default App;
