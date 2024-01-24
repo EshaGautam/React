bf22112b47bda961191f6dd1fdb1ca006288c356
Esha Gautam <eshagautam0@gmail.com>
2024-01-24 18:22:58 +0530

    passing data using props

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,18 +1,13 @@
  import './ExpenseItem.css'
 
-function ExpenseItem(){
-
-  const expenseDate = new Date(2024,0,24);
-  const expenseName = 'Car Insurance';
-  const expensePrice = "10 rs";
-  const expenseLocation = "Delhi"
+function ExpenseItem(props){
     return (
       <div className="expense-item">
-        <div>{expenseDate.toDateString()}</div>
+        <div>{props.date.toDateString()}</div>
         <div className="expense-item__description">
-          <h2>{expenseName} </h2>
-          <div className='expense-item__location'>{expenseLocation}</div>
-          <div className="expense-item__price ">{expensePrice} </div>
+          <h2>{props.title} </h2>
+          <div className='expense-item__location'>{props.location}</div>
+          <div className="expense-item__price ">{props.amount} </div>
         </div>
       </div>
     );
