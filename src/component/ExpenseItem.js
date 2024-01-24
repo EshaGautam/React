ce110d512b52fed337530ef923596b857db48c93
Esha Gautam <eshagautam0@gmail.com>
2024-01-24 19:09:57 +0530

    Making new custom Components

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,14 +1,16 @@
  import './ExpenseItem.css'
+ import ExpenseDate from './ExpenseDate'; 
+ import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props){
+
+
+
     return (
       <div className="expense-item">
-        <div>{props.date.toDateString()}</div>
-        <div className="expense-item__description">
-          <h2>{props.title} </h2>
-          <div className='expense-item__location'>{props.location}</div>
-          <div className="expense-item__price ">{props.amount} </div>
-        </div>
+       <ExpenseDate date = {props.date}/>
+       <ExpenseDetails title ={props.title} amount={props.amount} location={props.location}/>
+       
       </div>
     );
 }
