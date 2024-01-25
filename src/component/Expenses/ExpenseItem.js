beb599b685e57a2266c78da868d7c9a0792d335d
Esha Gautam <eshagautam0@gmail.com>
2024-01-25 16:58:59 +0530

    Added a button to change Expense

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,21 +1,27 @@
+ 
+import{ useState } from "react";
  import './ExpenseItem.css'
  import ExpenseDate from './ExpenseDate'; 
  import ExpenseDetails from './ExpenseDetails';
  import Card from '../UI/Card';
 
 function ExpenseItem(props){
-  const deleteExp =()=>{
-    console.log("Expense Deleted")
-  }
+
+  const [amount,setAmount] = useState(props.amount);
+
+  const ChangeExp =()=>{setAmount("100")}
+
+  const deleteExp =()=>{ console.log("Expense Deleted")}
     return (
    
         <Card className="expense-item">
           <ExpenseDate date={props.date} />
           <ExpenseDetails
             title={props.title}
-            amount={props.amount}
+            amount={amount}
             location={props.location}
           />
+          <button onClick = {ChangeExp}>Change-Expense</button>
           <button onClick={deleteExp}>Delete-Expense</button>
         </Card>
      
