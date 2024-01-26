6ef31c9256abe0cbe32a48c40f1fc8264415f383
Esha Gautam <eshagautam0@gmail.com>
2024-01-26 20:21:23 +0530

    Added conditionals

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -15,16 +15,39 @@ function Expenses(props){
       return expense.date.getFullYear().toString() === filteredYear
     })
 
+  // Adding some logic and conditions if the year we not made anyexpense in selected year
+
+   let expenseContent = <p>No Expense Found</p>
+
+   //if it has some content this will run 
+
+   if(filteredExpenses.length >0){  
+    expenseContent = filteredExpenses.map((expenses) => (
+       <ExpenseItem
+         key={expenses.id}
+         title={expenses.title}
+         amount={expenses.amount}
+         location={expenses.location}
+         date={expenses.date}
+       />
+     ));
+   }
+ 
+if (filteredExpenses.length === 1) {
+      expenseContent = (
+        <>
+          {expenseContent}
+          <p>Only a single Expense here. Please add more...</p>
+        </>
+      );
+    }
+  
+
+
     return (
       <Card className="expenses">
       <ExpenseFilter selected ={filteredYear} onYearChange={filterChangedHandler}/>
-      {filteredExpenses.map((expenses)=><ExpenseItem
-        key = {expenses.id}
-        title={expenses.title}
-        amount={expenses.amount}
-        location={expenses.location}
-        date={expenses.date}
-      />)}
+      {expenseContent}
       </Card>
     );
 }
